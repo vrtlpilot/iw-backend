07f197bfb58ba2902700b6091e16bfd43e511fad
UPavel <pavel32131@gmail.com>
2018-08-15 16:54:32 +0300

    refactored auth.js

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,17 +1,8 @@
-/* import * as jsonwebtoken from 'jsonwebtoken'; */
 import User from '../models/user';
 import * as crypto from 'crypto';
 import { Hash } from 'crypto';
-
-/*
-const secret = process.env.PWD_SEED || 'secret';
-
-// Create JWT token.
-function createToken(userData, options): string {
-  const { secret } = options;
-  return jsonwebtoken.sign(userData, secret);
-}
-*/
+import * as passport from 'koa-passport';
+import { Strategy as LocalStrategy } from 'passport-local'
 
 // Digest a given data parameter.
 function createDigest(data): string {
@@ -19,36 +10,6 @@ function createDigest(data): string {
   hash.update(data);
   return hash.digest('hex');
 }
-
-/*
-// Signup request definition.
-export async function signUp(firstName: string, lastName: string, email: string, password: string): Promise<string> {
-  const userData = {
-    name: `${firstName} ${lastName}`,
-    email,
-    pwd: createDigest(password)
-  }
-  const user = new User(userData);
-  const { _id, name } = (await user.save()) as any;
-  return createToken({ id: _id, name }, { secret });
-}
-
-// Login request definition.
-export async function login(email: string, password: string): Promise<string> {
-  const user = await User.findOne({ email }) as any;
-  if (!user) {
-    throw new IWError(401, `Cannot find user with email: ${email}`);
-  }
-  const isValidPassword = createDigest(password) === user.pwd;
-  if (!isValidPassword) {
-    throw new IWError(401, `Incorrect password for user: ${user.name}`);
-  }
-  return createToken({ id: user._id, name: user.name }, { secret });
-}
- */
-
-import * as passport from 'koa-passport';
-import { Strategy as LocalStrategy } from 'passport-local'
 
 passport.serializeUser((user: any, done) => {
   done(null, user._id);
@@ -93,7 +54,7 @@ passport.use('local-login', new LocalStrategy({
     }
     const isValidPassword = createDigest(password) === user.pwd;
     if (!isValidPassword) {
-      //  new IWError(401, `Incorrect password for user: ${user.name}`);
+      // new IWError(401, `Incorrect password for user: ${user.name}`);
       return done(null, false);
     }
     return done(null, user);
@@ -102,11 +63,6 @@ passport.use('local-login', new LocalStrategy({
 
 
 export default function(router) {
-  /* router.post('/signup', passport.authenticate('local-signup',), async (ctx) => {
-    const user = ctx.state.user;
-    ctx.body = { user };
-  }); */
-
   router.post('/signup', async (ctx, next) => {
     await passport.authenticate('local-signup', async (err, user) => {
       if (err) {
@@ -118,12 +74,6 @@ export default function(router) {
       }
     })(ctx, next);
   });
-
-  /* router.post('/login', passport.authenticate('local-login'), (ctx) => {
-      const user = ctx.state.user;
-      ctx.body = { name: user };
-    }
-  ); */
 
   router.post('/login', async (ctx, next) => {
     await passport.authenticate('local-login', async (err, user) => {
@@ -143,12 +93,4 @@ export default function(router) {
     await ctx.logout();
     ctx.body = 'logout';
   });
-
-  router.get('/success', async (ctx) => {
-    ctx.body = 'success';
-  });
-
-  /* router.get('/fail', async (ctx) => {
-    ctx.body = 'fail'
-  }) */
 }
